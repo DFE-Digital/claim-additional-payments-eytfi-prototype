fba4227b0cdf142a05c7848550a304495816017f
Richard Lynch <rjlynchdev@gmail.com>
2026-03-31 15:47:26 +0100

    Initial pages

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,23 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.get('/check-nursery', (req, res) => {
+  const { nursery } = req.query
+
+  if (nursery === 'ineligible-nursery') {
+    return res.redirect('/ineligible-nursery')
+  }
+
+  return res.redirect('/one-login-start')
+})
+
+router.get('/check-teacher-reference-number', (req, res) => {
+  const { hasTeacherReferenceNumber } = req.query
+
+  if (hasTeacherReferenceNumber === 'no') {
+    return res.redirect('/teacher-auth-no-teacher-reference-number.html')
+  }
+
+  return res.redirect('/teacher-auth-record-match')
+})
