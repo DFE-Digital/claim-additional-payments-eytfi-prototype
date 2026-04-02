f68524be8bac0370130caf2a2ef77dfb82cdb505
Richard Lynch <rjlynchdev@gmail.com>
2026-04-02 16:30:41 +0100

    Some more pages
    Not great git discipline here! I'll probably squash everything into a
    single commit as there's not any useful information in these commit
    messages!

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,26 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
+const uploadedDocumentPreview = {
+  fileName: 'document.jpg',
+  fileSize: '2MB',
+  imagePath: '/public/images/example-payslip.png'
+}
+
+function buildUploadedDocumentRows(uploadedDocuments) {
+  return uploadedDocuments.map((document, index) => ([
+    {
+      text: `File ${index + 1}`
+    },
+    {
+      html: `<a class="govuk-link" href="${document.imagePath}" target="_blank" rel="noreferrer noopener">${document.fileName}</a>, ${document.fileSize}`
+    },
+    {
+      html: '<a class="govuk-link" href="#">Delete</a>'
+    }
+  ]))
+}
+
 router.get('/check-nursery', (req, res) => {
   const { nursery } = req.query
 
@@ -26,4 +46,61 @@ router.get('/check-teacher-reference-number', (req, res) => {
   }
 
   return res.redirect('/teacher-auth-record-match')
+})
+
+router.post('/confirm-where-you-work-uploaded', (req, res) => {
+  req.session.data.pendingUploadedDocument = uploadedDocumentPreview
+
+  return res.redirect('/confirm-where-you-work-uploaded')
+})
+
+router.get('/check-uploaded-file', (req, res) => {
+  const { isFileCorrect } = req.query
+
+  if (isFileCorrect === 'no') {
+    req.session.data.pendingUploadedDocument = null
+    return res.redirect('/confirm-where-you-work')
+  }
+
+  const uploadedDocuments = req.session.data.uploadedDocuments || []
+  const pendingUploadedDocument = req.session.data.pendingUploadedDocument || uploadedDocumentPreview
+
+  req.session.data.uploadedDocuments = [...uploadedDocuments, pendingUploadedDocument]
+  req.session.data.pendingUploadedDocument = null
+
+  return res.redirect('/confirm-where-you-work-uploaded-documents')
+})
+
+router.get('/confirm-where-you-work-uploaded-documents', (req, res) => {
+  const uploadedDocuments = req.session.data.uploadedDocuments || [uploadedDocumentPreview]
+
+  return res.render('confirm-where-you-work-uploaded-documents', {
+    uploadedDocumentRows: buildUploadedDocumentRows(uploadedDocuments)
+  })
+})
+
+router.get('/check-another-document', (req, res) => {
+  const { uploadAnotherDocument } = req.query
+
+  if (uploadAnotherDocument === 'yes') {
+    return res.redirect('/confirm-where-you-work')
+  }
+
+  return res.redirect('/confirm-where-you-work-uploaded-documents-confirmation')
+})
+
+router.get('/check-payment-acceptance', (req, res) => {
+  const { acceptPayment } = req.query
+
+  if (acceptPayment === 'no') {
+    return res.redirect('/payment-rejected')
+  }
+
+  return res.redirect('/how-we-will-use-your-information')
+})
+
+router.get('/start-new-claim', (req, res) => {
+  req.session.data = {}
+
+  return res.redirect('/')
 })
