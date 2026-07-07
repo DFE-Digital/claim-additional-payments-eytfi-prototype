69050a0cfb0ba05ad8d7fe82cd57148198c5ee9e
alexleedfe <alex.lee@education.gov.uk>
2026-07-07 15:31:31 +0100

    first draft of filters

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,4 +5,26 @@
 
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
+
+
+  const toggle = document.getElementById('toggle-filters');
+  const content = document.getElementById('filters-content');
+
+  toggle.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const isHidden = content.style.display === 'none';
+
+    if (isHidden) {
+      content.style.display = '';
+      toggle.textContent = 'Hide filters';
+      toggle.setAttribute('aria-expanded', 'true');
+    } else {
+      content.style.display = 'none';
+      toggle.textContent = 'Show filters';
+      toggle.setAttribute('aria-expanded', 'false');
+    }
+  });
+
 })
+
