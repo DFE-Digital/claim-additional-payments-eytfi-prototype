f655f46ce8c0f3e4ec174c64a8936a7cd26e0c85
alexleedfe <alex.lee@education.gov.uk>
2026-07-21 12:19:18 +0100

    making a start on autocomplete component

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,21 +10,55 @@ window.GOVUKPrototypeKit.documentReady(() => {
   const toggle = document.getElementById('toggle-filters');
   const content = document.getElementById('filters-content');
 
-  toggle.addEventListener('click', function (e) {
-    e.preventDefault();
+  if (toggle && content) {
+    toggle.addEventListener('click', function (e) {
+      e.preventDefault();
 
-    const isHidden = content.style.display === 'none';
+      const isHidden = content.style.display === 'none';
 
-    if (isHidden) {
-      content.style.display = '';
-      toggle.textContent = 'Hide filters';
-      toggle.setAttribute('aria-expanded', 'true');
-    } else {
-      content.style.display = 'none';
-      toggle.textContent = 'Show filters';
-      toggle.setAttribute('aria-expanded', 'false');
+      if (isHidden) {
+        content.style.display = '';
+        toggle.textContent = 'Hide filters';
+        toggle.setAttribute('aria-expanded', 'true');
+      } else {
+        content.style.display = 'none';
+        toggle.textContent = 'Show filters';
+        toggle.setAttribute('aria-expanded', 'false');
+      }
+    });
+}
+
+  const selectEl = document.querySelector('.schoolSelecter');
+
+  if (selectEl) {
+
+    accessibleAutocomplete.enhanceSelectElement({
+      autoselect: true,
+      confirmOnBlur: true,
+      defaultValue: "",
+      minLength: 3,
+      selectElement: selectEl
+    });
+
+    const queryStringParameters = window.location.search;
+    const previouslySubmitted = queryStringParameters.length > 0;
+
+    if (previouslySubmitted) {
+      const submittedEl = document.querySelector('.submitted');
+
+      if (submittedEl) {
+        submittedEl.classList.remove('submitted--hidden');
+
+        const params = new URLSearchParams(window.location.search);
+
+        const hideSchool = document.querySelector('.submitted__hide-school');
+        if (hideSchool) {
+          hideSchool.textContent = params.get('hide-school');
+        }
+      }
     }
-  });
+
+  }
 
 })
 
